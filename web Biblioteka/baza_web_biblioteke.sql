e6792bd2d337bfad1ec5e64ce164a5d77c3e4dac
Robert Peric <robertperic@outlook.com>
2019-03-15 13:26:23 +0100

    Dodavanje SQL baze podataka

---
+++ web Biblioteka/baza_web_biblioteke.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2019 at 01:21 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `baza_web_biblioteke`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `autor`
+--
+
+CREATE TABLE `autor` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `prezime` varchar(32) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `knjiga`
+--
+
+CREATE TABLE `knjiga` (
+  `id` int(11) NOT NULL,
+  `autor_id` int(11) NOT NULL,
+  `naziv` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `cijena` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `korisnik`
+--
+
+CREATE TABLE `korisnik` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `prezime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `korisnicko_ime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `lozinka` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `uloga_korisnika` enum('korisnik','admin','super_admin') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'korisnik'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+INSERT INTO `korisnik` (`id`, `ime`, `prezime`, `korisnicko_ime`, `lozinka`, `uloga_korisnika`) VALUES
+(1, 'Robert', 'Perić', 'RobertP', 'aabbcc33', 'super_admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `narudzba`
+--
+
+CREATE TABLE `narudzba` (
+  `id` int(11) NOT NULL,
+  `kupac_id` int(11) NOT NULL,
+  `knjiga_id` int(11) NOT NULL,
+  `datum_narudzbe` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `autor`
+--
+ALTER TABLE `autor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `knjiga`
+--
+ALTER TABLE `knjiga`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `autor_id` (`autor_id`);
+
+--
+-- Indexes for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `korisnicko_ime` (`korisnicko_ime`);
+
+--
+-- Indexes for table `narudzba`
+--
+ALTER TABLE `narudzba`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `knjiga_id` (`knjiga_id`),
+  ADD KEY `kupac_id` (`kupac_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `autor`
+--
+ALTER TABLE `autor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `narudzba`
+--
+ALTER TABLE `narudzba`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `autor`
+--
+ALTER TABLE `autor`
+  ADD CONSTRAINT `autor_ibfk_1` FOREIGN KEY (`id`) REFERENCES `knjiga` (`autor_id`);
+
+--
+-- Constraints for table `knjiga`
+--
+ALTER TABLE `knjiga`
+  ADD CONSTRAINT `knjiga_ibfk_1` FOREIGN KEY (`autor_id`) REFERENCES `autor` (`id`);
+
+--
+-- Constraints for table `narudzba`
+--
+ALTER TABLE `narudzba`
+  ADD CONSTRAINT `narudzba_ibfk_1` FOREIGN KEY (`knjiga_id`) REFERENCES `knjiga` (`id`),
+  ADD CONSTRAINT `narudzba_ibfk_2` FOREIGN KEY (`kupac_id`) REFERENCES `korisnik` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
