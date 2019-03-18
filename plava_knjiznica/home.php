9d8367d64b8f1c19d94ae6b57cab3c9df0f11a18
Robert Peric <robertperic@outlook.com>
2019-03-18 11:07:49 +0100

    zarsetak postavljanja home.php

--- plava_knjiznica/home.php
+++ plava_knjiznica/home.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+?>
+
+
 <html>
 
 <head>
@@ -7,7 +12,16 @@
 <body>
     <div class="wraper">
         <div class="container">
-            <form action="">
+            
+            <?php
+                if(isset($_SESSION['poruka_prijava']) && !empty($_SESSION['poruka_prijava']));
+            ?>
+            <h1>
+                <?php echo $_SESSION['poruka_prijava'];?>
+            </h1>
+            <?php $_SESSION['poruka_prijava'] == null; ?>
+            <?php endif; ?>
+            <form class="form" action="login.php" method="POST">
                 <input type="text" name="korisnickoIme" placeholder="korisnicko Ime">
                 <input type="password" name="lozinka" , placeholder="lozinka">
                 <button type="submit" id="login-button">PRIJAVA</button>
