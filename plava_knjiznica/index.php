23da19b85e2c4b92a501dd028935b4254025498a
Robert Peric <robertperic@outlook.com>
2019-03-19 09:44:28 +0100

    reorganizacija foldera prema MVC obrazcu

--- plava_knjiznica/index.php
+++ plava_knjiznica/index.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
 
-    include("controller/db_config.php");
-    include("utils.php");
+    include("config/db_config.php");
+    include("controllers/utils.php");
 
     $view = "login";
 
-    load_view("view/static/header.php", array("title" => "Prijava na sustav"));
-    load_view("view/static/navbar.html");
+    load_view("views/static/header.php", array("title" => "Prijava na sustav"));
+    load_view("views/static/navbar.php");
     //load_view("view/$view.php");
-    load_view("view/static/footer.php");
+    load_view("views/static/footer.php");
 ?>
