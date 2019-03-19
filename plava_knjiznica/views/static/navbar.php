5dc6e7ddeec4f3998f6ec01c25bb136968b7d09e
Robert Peric <robertperic@outlook.com>
2019-03-19 10:15:25 +0100

    uredjivanje styla navbara

--- plava_knjiznica/views/static/navbar.php
+++ plava_knjiznica/views/static/navbar.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand naslov" href="#" >Plava Knjižnica</a>
+    <a class="navbar-brand" href="#" id="naslov"><strong>Plava Knjižnica</strong></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">        <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Ponuda</a>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ponuda <i class="fas fa-book"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Profil</a>
+                <a class="nav-link" href="#">Profil <i class="fas fa-user-tie"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">informacije o Knjižnici</a>
+                <a class="nav-link" href="#">Informacije<i class="fas fa-info-circle"></i></a>
             </li>
         </ul>
     </div>
