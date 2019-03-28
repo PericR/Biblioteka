929507db18fb78e1fed064a6d512832cfbc738e2
Robert Peric <robertperic@outlook.com>
2019-03-28 09:55:06 +0100

    dodan pocetak rada na formi dodaj karticu

--- plava_knjiznica/views/dodaj_karticu.php
+++ plava_knjiznica/views/dodaj_karticu.php
@@ -1,6 +1,8 @@
 <div class="row">
     <div class="col-md-6 offset-md-3">
-        hello
+        <form method="POST" action="controllers/dodaj_karticu.php">
+            
+        </form>
     </div>
 
 </div>
